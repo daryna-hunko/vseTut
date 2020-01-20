ee8bfc79af8ebcf98a1e7c8804775e9fc0e82e0b
dhunko <daryna.hunko@bazaarvoice.com>
2020-01-20 13:14:14 +0200

    somehow it looks better

--- src/FormComp.js
+++ src/FormComp.js
@@ -2,14 +2,15 @@ import React, {useState} from 'react';
 import './FormComp.css';
 
 function FormComp(props) {
-
+  const [label, setLabel] = useState(props.label);
+  const [value, setValue] = useState(props.value);
 
   return (
     <form>
         <label>
-          {props.label}
+          {label}
         </label>
-        <input type="text" value={props.value} />
+        <input type="text" value={props.value} onChange={(e) => setValue(e.target.value)}/>
         <input type="submit" value="Submit"/>
     </form>
   );
