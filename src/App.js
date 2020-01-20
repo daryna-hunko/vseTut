3f99311c8ea0e765dd369188e5ecb511efb5427d
dhunko <daryna.hunko@bazaarvoice.com>
2020-01-20 13:02:32 +0200

    somehow it works via functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FormComp from './FormComp.js';
 import logo from './logo.svg';
 import './App.css';
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <FormComp label='Test text' value='Test value'/>
-    );
-  }
+function App(props) {
+  return (
+    <FormComp label='test label' value='some value'/>
+  );
 }
 
 export default App;
--- src/FormComp.js
+++ src/FormComp.js
@@ -1,25 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './FormComp.css';
 
-class FormComp extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: this.props.value};
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-    render() {
-      return (
-        <form>
-          <label>
-            {this.props.label}
-            <input type="text" value={this.state.value} />
-          </label>
-          <input type="submit" value="Submit" onChange={this.handleSubmit}/>
-        </form>
-      );
-    }
-  }
+function FormComp(props) {
+
+
+  return (
+    <form>
+        <label>
+          {props.label}
+        </label>
+        <input type="text" value={props.value} />
+        <input type="submit" value="Submit"/>
+    </form>
+  );
+}
 
 export default FormComp;
