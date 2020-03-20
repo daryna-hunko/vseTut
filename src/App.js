06d904d8c7d13e273fe50dea72cedb4195ae1b82
dhunko <daryna.hunko@bazaarvoice.com>
2020-03-20 11:20:39 +0200

    date picker - update: added input of the date and moved all logic into the component, left to fix the selecting date in controls in case input is valid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import FormComp from './FormComp.js';
-import DatePicker from './DatePickerComp.js';
+import DatePicker from './DatePicker.js';
 import styled from 'styled-components';
 
 const Form = styled.form`
@@ -14,42 +14,12 @@ const Form = styled.form`
 
 
 function App(props) {
-  let Days = [];
-  const Monthes = [1,2,3,4,5,6,7,8,9,10,11,12];
-  const Years = [];
   const [controlledValue, setControlledValue] = useState('some value');
   
-  const [controlledDays, setControlledDays] = useState(''),
-        [controlledMonthes, setControlledMonthes] = useState(''),
-        [controlledYears, setControlledYears] = useState('');
-        
-  for (let i = 2020; i > 1900; i--) {
-    Years.push(i);
-  }
-  for (let i = 1; i <= (daysInMonth(controlledMonthes, controlledYears) || 31); i++) {
-    Days.push(i);
-  }
-
 
   return (
     <>
-      <DatePicker 
-        Days={Days} 
-        Monthes={Monthes} 
-        Years={Years} 
-        onDaysChange={(e) => {
-          setControlledDays(e.target.value);
-          outputer(e.target.value, controlledMonthes, controlledYears);
-        }} 
-        onMonthesChange={(e) => {
-          setControlledMonthes(e.target.value);
-          outputer(controlledDays, e.target.value, controlledYears);
-        }} 
-        onYearsChange={(e) => {
-          setControlledYears(e.target.value);
-          outputer(controlledDays, controlledMonthes, e.target.value);
-        }} 
-      />
+      <DatePicker date={'12-12-2012'}/>
       <Form>
         <FormComp label={'test label'} value={controlledValue} onChange={(e) => setControlledValue(e.target.value)} onClick={() => alert('onClick')}/>
         <input type='submit' value='Submit'/>
@@ -58,14 +28,5 @@ function App(props) {
   );
 }
 
-function outputer(a, b, c) {
-  if (a.length !== 0 && b.length !== 0 && c.length !== 0) {
-    document.querySelector('.date-picker-output').innerText = a + ' ' + b + ' ' + c;
-  }
-}
-
-function daysInMonth (month = 1, year = 2020) { // Use 1 for January, 2 for February, etc.
-  return +(new Date(year, month, 0).getDate());
-}
 
 export default App;
---
+++ src/DatePicker.js
@@ -0,0 +1,127 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import SelectBuilder from './SelectBuilder.js';
+
+const Div = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  padding: 25px 0;
+  font-size: 24px;
+  border-bottom: 1px solid #000;
+`; 
+
+const OutputDiv = styled.div`
+  height: 25px;
+  padding: 25px 0;
+`; 
+
+const Input = styled.input`
+  margin-left: 10px;
+`; 
+const Label = styled.label`
+  font-size: 14px;
+`; 
+
+
+
+function DatePicker(props) {
+  const [controlledDays, setControlledDays] = useState(''),
+        [controlledMonthes, setControlledMonthes] = useState(''),
+        [controlledYears, setControlledYears] = useState(''),
+        [controlledDate, setControlledDate] = useState('');
+  let Days = [];
+  const Monthes = [1,2,3,4,5,6,7,8,9,10,11,12];
+  const Years = [];
+  for (let i = 2020; i > 1900; i--) {
+    Years.push(i);
+  }
+  for (let i = 1; i <= (daysInMonth(controlledMonthes, controlledYears) || 31); i++) {
+    Days.push(i);
+  }
+
+  let Date = (validateDate(props.date)) ? props.date : '';
+  
+
+  return (
+    <Div>
+      <Label>
+        Enter date or choose it: 
+        <Input className="date-picker-input" placeholder="dd-mm-yyyy" onChange={(e) => {
+          if (validateDate(e.target.value)) {
+            setControlledDate(e.target.value);
+            document.querySelector('.date-picker-output').innerText = e.target.value;
+          } else {
+            alertErr();
+          }
+        }}/>
+      </Label>
+
+      <div>
+        <SelectBuilder className='date-select' numbers={Days} onChange={(e) => {
+          setControlledDays(e.target.value);
+          outputer(e.target.value, controlledMonthes, controlledYears);
+        }} />
+        <SelectBuilder className='month-select' numbers={Monthes} onChange={(e) => {
+          setControlledMonthes(e.target.value);
+          outputer(controlledDays, e.target.value, controlledYears);
+        }} />
+        <SelectBuilder className='year-select' numbers={Years} onChange={(e) => {
+          setControlledYears(e.target.value);
+          outputer(controlledDays, controlledMonthes, e.target.value);
+        }} />
+      </div>
+      <OutputDiv className="date-picker-output">{Date}</OutputDiv>
+    </Div>
+  );
+}
+
+
+function outputer(a, b, c) {
+  if (a.length !== 0 && b.length !== 0 && c.length !== 0) {
+    document.querySelector('.date-picker-output').innerText = a + ' ' + b + ' ' + c;
+  } else {
+    alertErr();
+  }
+}
+
+function daysInMonth (month = 1, year = 2020) { // Use 1 for January, 2 for February, etc.
+  return +(new Date(year, month, 0).getDate());
+}
+
+function validateDate(date) {
+  let dateFormat = /^(0?[1-9]|[12][0-9]|3[01])[\-](0?[1-9]|1[012])[\-]\d{4}$/;
+  if(date.match(dateFormat)) {
+    const DateArr = date.split('-');
+
+    if (DateArr[2] > 1900 && DateArr[2] <= 2020) {
+      if (DateArr[1] > 0 && DateArr[1] <= 12) {
+        if (DateArr[0] > 0 && DateArr[0] <= daysInMonth (DateArr[1], DateArr[2])) {
+         /* setControlledDate(DateArr[0]);
+          setControlledMonthes(DateArr[1]);
+          setControlledYears(DateArr[2]);
+          selectOption('.date-select', controlledDays);
+          selectOption('.month-select', controlledMonthes);
+          selectOption('.year-select', controlledYears);*/
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+function alertErr() {
+  document.querySelector('.date-picker-output').innerText = 'didn\`t get any valid data';
+}
+
+function selectOption(className, value) {
+  if (document.querySelector(className) != undefined) {
+      const select = document.querySelector(className).getElementsByTagName('option');
+    for (let i = 0; i < select.length; i++) {
+        if (select[i].value == value.toString()) select[i].selected = true;
+    }
+  }
+}
+  
+export default DatePicker;
