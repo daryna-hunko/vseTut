ddfdccfff1355b97624fd100db44bfd816aaff9c
dhunko <daryna.hunko@bazaarvoice.com>
2020-03-24 13:48:40 +0200

    date picker - update

--- src/App.js
+++ src/App.js
@@ -9,17 +9,24 @@ const Form = styled.form`
   margin: 25px auto;
   font-size: 24px;
 `; 
-
+const OutputDiv = styled.div`
+  height: 25px;
+  text-align: center;
+  padding: 25px 0;
+  border-bottom: 1px solid #000;
+`; 
 
 
 
 function App(props) {
   const [controlledValue, setControlledValue] = useState('some value');
-  
+  const [birthday, setBirthday] = useState(new Date().toISOString().split('T')[0]); // текущая дата как начальное значение
+  //const isInvalid = isNaN(new Date(birthday)); // true для даты Invalid Date
 
   return (
     <>
-      <DatePicker date={'12-12-2012'}/>
+      <DatePicker date={birthday} onChange={'d'}/>
+      <OutputDiv className="date-picker-output">{birthday}</OutputDiv>
       <Form>
         <FormComp label={'test label'} value={controlledValue} onChange={(e) => setControlledValue(e.target.value)} onClick={() => alert('onClick')}/>
         <input type='submit' value='Submit'/>
--- src/DatePicker.js
+++ src/DatePicker.js
@@ -7,70 +7,41 @@ const Div = styled.div`
   margin: 0 auto;
   padding: 25px 0;
   font-size: 24px;
-  border-bottom: 1px solid #000;
-`; 
-
-const OutputDiv = styled.div`
-  height: 25px;
-  padding: 25px 0;
-`; 
-
-const Input = styled.input`
-  margin-left: 10px;
-`; 
-const Label = styled.label`
-  font-size: 14px;
 `; 
 
 
 
 function DatePicker(props) {
-  const [controlledDays, setControlledDays] = useState(''),
-        [controlledMonthes, setControlledMonthes] = useState(''),
-        [controlledYears, setControlledYears] = useState('');
-        //[controlledDate, setControlledDate] = useState('');
+  let bday = props.date.split('-');
+  const [controlledDays, setControlledDays] = useState(bday[2]),
+        [controlledMonthes, setControlledMonthes] = useState(bday[1]),
+        [controlledYears, setControlledYears] = useState(bday[0]);
   let Days = [];
-  const Monthes = [1,2,3,4,5,6,7,8,9,10,11,12];
+  const Monthes = ['01','02','03','04','05','06','07','08','09','10','11','12'];
   const Years = [];
   for (let i = 2020; i > 1900; i--) {
     Years.push(i);
   }
   for (let i = 1; i <= (daysInMonth(controlledMonthes, controlledYears) || 31); i++) {
-    Days.push(i);
+    (i < 10) ? Days.push('0' + i) : Days.push(i);
   }
-
-  let Date = (validateDate(props.date)) ? props.date : '';
-  
 
   return (
     <Div>
-      <Label>
-        Enter date or choose it: 
-        <Input className="date-picker-input" placeholder="dd-mm-yyyy" onChange={(e) => {
-          if (validateDate(e.target.value)) {
-            //setControlledDate(e.target.value);
-            document.querySelector('.date-picker-output').innerText = e.target.value;
-          } else {
-            alertErr();
-          }
-        }}/>
-      </Label>
-
       <div>
         <SelectBuilder className='date-select' numbers={Days} onChange={(e) => {
           setControlledDays(e.target.value);
-          outputer(e.target.value, controlledMonthes, controlledYears);
+          outputer(controlledYears,  controlledMonthes, e.target.value);
         }} />
         <SelectBuilder className='month-select' numbers={Monthes} onChange={(e) => {
           setControlledMonthes(e.target.value);
-          outputer(controlledDays, e.target.value, controlledYears);
+          outputer(controlledYears, e.target.value, controlledDays);
         }} />
         <SelectBuilder className='year-select' numbers={Years} onChange={(e) => {
           setControlledYears(e.target.value);
-          outputer(controlledDays, controlledMonthes, e.target.value);
+          outputer(e.target.value, controlledMonthes, controlledDays);
         }} />
       </div>
-      <OutputDiv className="date-picker-output">{Date}</OutputDiv>
     </Div>
   );
 }
@@ -79,8 +50,6 @@ function DatePicker(props) {
 function outputer(a, b, c) {
   if (a.length !== 0 && b.length !== 0 && c.length !== 0) {
     document.querySelector('.date-picker-output').innerText = a + ' ' + b + ' ' + c;
-  } else {
-    alertErr();
   }
 }
 
@@ -88,24 +57,5 @@ function daysInMonth (month = 1, year = 2020) { // Use 1 for January, 2 for Febr
   return +(new Date(year, month, 0).getDate());
 }
 
-function validateDate(date) {
-  let dateFormat = /^(0?[1-9]|[12][0-9]|3[01])[\-](0?[1-9]|1[012])[\-]\d{4}$/;
-  if(date.match(dateFormat)) {
-    const DateArr = date.split('-');
-
-    if (DateArr[2] > 1900 && DateArr[2] <= 2020) {
-      if (DateArr[1] > 0 && DateArr[1] <= 12) {
-        if (DateArr[0] > 0 && DateArr[0] <= daysInMonth (DateArr[1], DateArr[2])) {
-          return true;
-        }
-      }
-    }
-  }
-  return false;
-}
-
-function alertErr() {
-  document.querySelector('.date-picker-output').innerText = 'didn\`t get any valid data';
-}
   
 export default DatePicker;
