71cb8b4126c357d27f87f52e8023d9c7ac505ec5
dhunko <dhun@softserveinc.com>
2020-08-19 21:31:00 +0300

    XOGame - more routers

--- src/XOGame/XOGameHistory.js
+++ src/XOGame/XOGameHistory.js
@@ -4,27 +4,30 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useLocation
 } from "react-router-dom";
 import XOGameField from './XOGameField.js';
 
 function XOGameHistory(props) {
   let gamesHistory = (props.gamesHistory != undefined) ? <ul>{props.gamesHistory.map((el, i) => <li key={i}>{el.winningDate + el.winnerIs}</li>)}</ul> : '',
-  winnigField = (props.gamesHistory.length != 0) ? props.gamesHistory[props.gamesHistory.length - 1].winnigField : '';
+    winnigField = (props.gamesHistory.length != 0) ? props.gamesHistory[props.gamesHistory.length - 1].winnigField : '',
+    useQuery = () => new URLSearchParams(useLocation().search),
+    query = useQuery();
 
   return (
     <>
      <Router>
         <br/>
         &nbsp;&nbsp;
-        <Link to="/lastWinningGame">Last Winning Game</Link>
+        <Link to={"/history/LastWinningGame?winnigField=" + winnigField}>Last Winning Game</Link>
 
         <Switch>
           <Route exact path="/history">
             {gamesHistory}
           </Route>
-          <Route exact path="/lastWinningGame">
-            <XOGameField field={winnigField}/>
+          <Route path={"/history/LastWinningGame?winnigField=" + winnigField}>
+            <XOGameField field={query.get("winnigField")}/>
           </Route>
         </Switch>
 
